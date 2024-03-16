e2f7e8a746542c81b9cf4c7099b687587910c8a6
moprules <rav-navini-gego-cutropal@yandex.ru>
2024-03-16 20:43:43 +0300

    Ускорили поиск по книг, завернув каждый запрос в Deferred<Book> объект

--- app/src/main/java/ru/oraora/books/data/repository/BookRepository.kt
+++ app/src/main/java/ru/oraora/books/data/repository/BookRepository.kt
@@ -1,5 +1,8 @@
 package ru.oraora.books.data.repository
 
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import ru.oraora.books.data.models.Book
 import ru.oraora.books.data.network.BookApiService
 
@@ -10,12 +13,24 @@ interface BookRepository {
 class NetworkBookRepository(
     private val bookApiService: BookApiService,
 ): BookRepository {
-    override suspend fun getBooks(query: String): List<Book> {
-        val books =  mutableListOf<Book>()
+    override suspend fun getBooks(query: String): List<Book> = coroutineScope {
+        // список id книг для поискового запроса query
         val ids: List<String> = bookApiService.getIdBooks(query)
-        ids.forEach {id ->
-            books.add(bookApiService.getBookById(id))
+        // Список deferred запросов информации по каждому id
+        // Как только вызываем async - запрос выполняется, не блокируя другие запросы
+        val requests: List<Deferred<Book>> = ids.map {
+            async { bookApiService.getBookById(it) }
         }
-        return books
+        // Результирующий списко книг. Чтобы получить ответ по каждому запрсоу его нужно ожидать
+        val books: List<Book> = requests.map { it.await() }
+        books
     }
+
+//    override suspend fun getBooks(query: String): List<Book> {
+//        // список id книг для поискового запроса query
+//        val ids: List<String> = bookApiService.getIdBooks(query)
+//        // Результирующий списко книг. Чтобы получить ответ по каждому запрсоу его нужно ожидать
+//        val books: List<Book> = ids.map { bookApiService.getBookById(it) }
+//        return books
+//    }
 }
